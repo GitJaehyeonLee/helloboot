6dd8d3301c3a24f110f7ebdad31e4e556ea8307d
jaylee <kerneltrek@gmail.com>
2023-02-02 20:57:06 +0900

    Annotaion 추가하여 동작가능하도록 수정

--- src/main/java/com/example/helloboot/HelloController.java
+++ src/main/java/com/example/helloboot/HelloController.java
@@ -1,11 +1,15 @@
 package com.example.helloboot;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Objects;
 
-@RestController
+// Dispatcher 의 경우, 기본적으로 클래스를 뒤진 후 메서드를 뒤진다.
+// 그럼으로 클래스 레벨에도 붙이는 것이 좋다.
+@RequestMapping("/hello")
 public class HelloController {
     private final HelloService helloService;
 
@@ -13,7 +17,10 @@ public class HelloController {
         this.helloService = helloService;
     }
 
-    @GetMapping("/hello")
+    // RestController 인 경우, Body 는 ResponseBody 로 고정
+    // 아닌 경우, 템플릿을 뒤져서 출력하는 편임.
+    @GetMapping
+    @ResponseBody
     public String hello(String name) {
         return helloService.sayHello(Objects.requireNonNull(name));
     }
