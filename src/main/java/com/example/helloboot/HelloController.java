7264a6b43e479ca24367b332023b925f3a6c5654
jaylee <kerneltrek@gmail.com>
2023-02-02 21:19:58 +0900

    Annotation 추가 및 활용

--- src/main/java/com/example/helloboot/HelloController.java
+++ src/main/java/com/example/helloboot/HelloController.java
@@ -1,5 +1,7 @@
 package com.example.helloboot;
 
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,7 +11,11 @@ import java.util.Objects;
 
 // Dispatcher 의 경우, 기본적으로 클래스를 뒤진 후 메서드를 뒤진다.
 // 그럼으로 클래스 레벨에도 붙이는 것이 좋다.
-@RequestMapping("/hello")
+// 메테 에노테이션 (에노테이션 위에 에노테이션) 에 컴포넌트 에노테이션이 포함된 경우 이 메타 에노테이션을 붙이면, 컴포넌트로 인식해서 진행한다.
+// 보통 정하는 경우는, 이 컴포넌트가 어떤 대상인지 명시하기 위해서 주로 사용한다.
+
+// RestController 와 controller 의 차이점은 RestContoller는 컨트롤러를 상속 받아 활용하며, 추가적으로  Responsebody 를 기록한다.
+@RestController
 public class HelloController {
     private final HelloService helloService;
 
@@ -19,8 +25,7 @@ public class HelloController {
 
     // RestController 인 경우, Body 는 ResponseBody 로 고정
     // 아닌 경우, 템플릿을 뒤져서 출력하는 편임.
-    @GetMapping
-    @ResponseBody
+    @GetMapping("/hello")
     public String hello(String name) {
         return helloService.sayHello(Objects.requireNonNull(name));
     }
--- src/main/java/com/example/helloboot/HellobootApplication.java
+++ src/main/java/com/example/helloboot/HellobootApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.http.HttpHeaders;
@@ -23,25 +24,12 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 // 컴포넌트를 표기하여, 현재 클래스 내에 빈이 있음을 인지
+// ComponentScan 을 표기하여, 하위 패키지 대상 전체를 컴포넌트로 표기함
+// 단점은 컴포넌트가 많아질떄 의존 관계를 쉽게 판단하기 어려움.
 @Configuration
+@ComponentScan
 public class HellobootApplication {
-    // 팩토리 메서드 생성 (빈의 초기화 등을 볃도로 진행하는 것으로, 기존에는 빈이 등록되면 자동으로 맵핑하나, 복잡한 경우에는 별도로 하는 경우도 있음)
-    // Bean 을 선언하여 이 함수는 호출되어야 한다고 표기
-    @Bean
-    public HelloController helloController(HelloService helloService) {
-        return new HelloController(helloService);
-    }
-
-    @Bean
-    public HelloService helloService() {
-        return new SimpleHelloService();
-    }
-
     public static void main(String[] args) {
-        // 팩토리 메서드 : 오브젝트를 생성하는 메소드를 이야기 한다.
-        // Spring Container 생성 Context 가 Container 임
-        // 컨테이너 생성
-        // Srping 컨테이너를 생성할 때, 서블릿 컨테이너 및 프론트 컨트롤러가 초기화 되는 것이 좋은 패턴이다.
         AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
             @Override
             protected void onRefresh() {
